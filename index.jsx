f4e68de5f5788cdf40ac090d94ad6c3e95429732
Jarred de Beer <jarr2000@gmail.com>
2021-08-26 19:39:53 +0200

    initial

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Table} from 'semantic-ui-react';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render("hello", rootElement);
